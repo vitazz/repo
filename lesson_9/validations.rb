b4e58a40d88f4339c211ecac2d889da2e6ee1390
vitazz <vitazz25@rambler.ru>
2016-01-26 21:09:31 +0300

    lesson 9

--- lesson_9/validations.rb
+++ lesson_9/validations.rb
@@ -5,32 +5,59 @@ module Validations
   end
 
   module ClassMethods
+    # def validate(name, *args)
+    #   @validate_data ||= {}
+    #   @validate_data[name] = *args
+    #   puts @validate_data.inspect
+    # end
+
     def validate(name, *args)
-      @validate_data = {}
-      @validate_data[name] = *args
-      puts @validate_data.inspect
+      validates_name = '@validates'
+      instance_variable_set(validates_name, {}) unless instance_variable_defined?(validates_name)
+      instance_variable_get(validates_name)[name] = *args
     end
   end
 
   module InstanceMethods
+    # def validate!
+    #   self.class.instance_variable_get("@validate_data").each do |name, args|
+    #     @name = instance_variable_get("@#{name}")
+    #     @type = args.last
+    #     send args[0]
+    #   end
+    # end
     def validate!
-      self.class.instance_variable_get("@validate_data").each do |name, args|
-        @name = instance_variable_get("@#{name}")
-        @type = args.last
-        send args[0]
+      self.class.instance_variable_get('@validates').each do |name, args|
+        send("validate_#{args[0]}", name, *args[1, args.size])
       end
+      true
     end
 
-    def format
-      fail 'Wrong format' if @name !~ @type
+    # def format
+    #   fail 'Wrong format' if @name !~ @type
+    # end
+    #
+    # def presence
+    #   fail 'Empty name' if @name.nil?
+    # end
+    #
+    # def type
+    #   fail 'Wrong class' if @name_tmp.is_a?(@type)
+    # end
+
+    def validate_presence(name, message='Argument is empty string')
+      value = instance_variable_get("@#{name}")
+      fail ArgumentError, message if value.nil? || value.empty?
     end
 
-    def presence
-      fail 'Empty name' if @name.nil?
+    def validate_format(name, format, message='Invalid format')
+      value = instance_variable_get("@#{name}")
+      fail ArgumentError, message unless value =~ format
     end
 
-    def type
-      fail 'Wrong class' if @name_tmp.is_a?(@type)
+    def validate_type(name, type, message='Invalid type')
+      value = instance_variable_get("@#{name}")
+      fail ArgumentError, message unless value.instance_of? type
     end
 
     def valid?
